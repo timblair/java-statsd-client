0a23c7799111e5e09ccf9bd09701cfee9cee60f1
Tim Blair <tim@bla.ir>
2012-08-01 17:34:18 +0100

    Basic testing framework in place.

--- src/test/java/com/timgroup/statsd/StatsDClientTest.java
+++ src/test/java/com/timgroup/statsd/StatsDClientTest.java
@@ -3,43 +3,57 @@ package com.timgroup.statsd;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 
+import java.net.SocketException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StatsDClientTest {
 
     private static final int STATSD_SERVER_PORT = 17254;
-    private final StatsDClient client = new StatsDClient("my.prefix", "localhost", STATSD_SERVER_PORT);
+    private static final StatsDClient client = new StatsDClient("my.prefix", "localhost", STATSD_SERVER_PORT);
+    private static final List<String> received = new ArrayList<String>();
+    private static DatagramSocket server;
 
-    @After
-    public void stop() throws Exception {
+    @BeforeClass
+    static public void start() throws Exception {
+        server = new DatagramSocket(STATSD_SERVER_PORT);
+        new Thread(new Runnable() {
+            @Override public void run() {
+                while (!server.isClosed()) {
+                    try {
+                        final DatagramPacket packet = new DatagramPacket(new byte[256], 256);
+                        server.receive(packet);
+                        String in = new String(packet.getData()).trim();
+                        received.add(in);
+                        System.out.println(in);
+                    } catch (Exception e) { }
+                }
+            }
+        }).start();
+    }
+
+    @AfterClass
+    static public void stop() throws Exception {
+        server.close();
         client.stop();
     }
 
+    @After
+    public void clearIncoming() throws Exception {
+        received.clear();
+    }
+
     @Test(timeout=5000L) public void
-    sends_records_to_statsd() throws Exception {
-        final List<String> messagesReceived = new ArrayList<String>();
-        final DatagramSocket server = new DatagramSocket(STATSD_SERVER_PORT);
-        
-        new Thread(new Runnable() {
-            @Override public void run() {
-                try {
-                    final DatagramPacket packet = new DatagramPacket(new byte[256], 256);
-                    server.receive(packet);
-                    messagesReceived.add(new String(packet.getData()).trim());
-                    server.close();
-                } catch (Exception e) { }
-            }
-        }).start();
-        
-        client.incrementCounter("blah");
-        while (messagesReceived.isEmpty()) { Thread.sleep(50L); }
-        
-        assertThat(messagesReceived, contains("my.prefix.blah:1|c"));
+    increments_correctly() throws Exception {
+        client.increment("blah");
+        while (received.isEmpty()) { Thread.sleep(50L); }
+        assertThat(received, contains("my.prefix.blah:1|c"));
     }
 }
